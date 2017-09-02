01adf825364a62bdfb1fa033d48927b92d1a8c87
dhollaway <hollaway.david@gmail.com>
2017-09-02 13:17:28 -0700

    95%. Need to compare current word array with individual key's pressed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,11 @@ var guessesRemaining = 5;
 var underscores = "";
 
 //Create word to be guessed.
-var easyWordBank = ["cat", "dog", "frog", "cow", "pig"];
-var lettersGuessed = [];
-var currentWordToGuess = "";
+var easyWordBank = ["cat", "dog", "can", "cow", "pig"];
+var lettersGuessed = []; //When user inputs letter
+var currentWordToGuess = ""; //Word that is generated from easyWordBank.
+var answerArray = [];
+
 var endOfGameText = "";
 
 //experimental
@@ -18,7 +20,11 @@ var secondLetter = "";
 var thirdLetter = "";
 var fourthLetter = "";
 
-//ar gameSound = new sound("../sounds/backgroundGameSound.mp3");
+//Sounds
+var soundCorrect = new Audio();
+    soundCorrect.src = "assets/sounds/ding.mp3";
+var soundIncorrect = new Audio();
+    soundIncorrect.src = "assets/sounds/horn_wrong.mp3";
 
 //EVENT LISTENER: Create event listener for any key that is pressed.
 document.onkeyup = function(event)
@@ -27,7 +33,7 @@ document.onkeyup = function(event)
     keyPressed = event.key; //Determines which key is pressed
     //Determine if this is the first time the user clicks the button
     keyPressedCount++;
-    console.log(keyPressedCount);
+    console.log("This is the key that was pressed: " +keyPressedCount);
     if(keyPressedCount == 1)
     {
         startGame();
@@ -40,9 +46,7 @@ document.onkeyup = function(event)
 }
 
 //FUNCTIONS
-
-    function  playSound(){
-    gameSound.play();
+    function preloadSound(){
      }
 
      //Generates the word
@@ -50,7 +54,8 @@ document.onkeyup = function(event)
     {
         var calculateWordPosition = Math.floor(Math.random() * easyWordBank.length);
         currentWordToGuess = easyWordBank[calculateWordPosition];
-        console.log(currentWordToGuess);
+        console.log("This is the current word to guess: " +currentWordToGuess);
+        answerArrayFunction();
     }
 
     function startGame()
@@ -58,7 +63,20 @@ document.onkeyup = function(event)
         document.getElementById("preGameText").innerHTML = "Guess the letter!";
         generateWord();
         fillTextFields();
+        preloadSound();
     }
+    //Pushes the word generated into an answer array to compare.
+    function answerArrayFunction()
+    {
+        for(var i =0; i< currentWordToGuess.length; i++)
+        {
+            answerArray.push(currentWordToGuess.charAt(i));
+            console.log("This is each letter: "+answerArray);
+        }
+
+
+    }
+
 
     //fills the text fields
     function fillTextFields()
@@ -69,9 +87,10 @@ document.onkeyup = function(event)
         if(keyPressedCount == 1) {
             for (var i = 0; i < currentWordToGuess.length; i++) {
                 underscores = underscores.concat() + " ___ ,";
-                console.log(underscores);
+                //console.log(underscores);
             }
         }
+
 
         //Check for win ??????????????????????????????????????
         firstLetter = currentWordToGuess.charAt(0);
@@ -82,15 +101,28 @@ document.onkeyup = function(event)
         //How do I check for to see if individual guesses/letters = the current word.
         //Add sound and a sprite sheet?
         guessesRemaining -=1;
-        console.log(guessesRemaining);
+        //console.log(guessesRemaining);
 
         //check the word inside
         checkEachLetter();
-
+        if(keyPressedCount > 1){
+                if(keyPressed == answerArray[0] || keyPressed == answerArray[1] || keyPressed == answerArray[2] )//what about words > 3?
+                {
+                    //play correct sound.
+                    console.log("You got a letter!" +keyPressed);
+                    soundCorrect.play();
+                }
+                else
+                {
+                    //play incorrect sound.
+                    console.log("That's the wrong answer");
+                    soundIncorrect.play();
+                }
+        }
         //Game is over
         if(guessesRemaining ==0)
         {
-            gameOver();
+            gameOverLose();
         }
         else{
             //document.getElementById("numberOfWins").innerHTML = " Number of wins:  " +wins;
@@ -98,6 +130,7 @@ document.onkeyup = function(event)
             document.getElementById("numberOfGuesses").innerHTML = "Guesses Remaining:  " +guessesRemaining;
             document.getElementById("lettersAlreadyGuessed").innerHTML = "Letter already guessed:  " +lettersGuessed;
             //append?
+            //console.log("Key press count: " +keyPressedCount);
         }
 
     }
@@ -117,17 +150,13 @@ document.onkeyup = function(event)
         // thirdLetter = currentWordToGuess.charAt(2);
         // fourthLetter = currentWordToGuess.charAt(3);
 
-        // console.log(firstLetter);
-        // console.log(secondLetter);
-        // console.log(thirdLetter);
-        // console.log(fourthLetter);
 
 
         //return firstLetter;
     }
 
 //game is over
-function gameOver()
+function gameOverLose()
 {
     endOfGameText = "The word is " +currentWordToGuess +" . Press any key to start over";
     document.getElementById("preGameText").innerHTML = endOfGameText;
@@ -148,6 +177,13 @@ function gameOver()
     //playAgain();
 }
 
+function gameOverWin()
+{
+
+
+
+
+}
 
 
     //User plays the game again.
